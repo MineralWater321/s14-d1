1cd343663616f3ebc530b0e54ae930f90e2b09d3
Edward Paler <paler.edward@gmail.com>
2021-10-15 14:02:14 +0800

    Declare variables

---
+++ index.js
@@ -0,0 +1,56 @@
+// Single line comments - ctrl + /
+
+/*
+	Multiline comments
+	ctrl + shift + /
+*/
+
+//Syntax and Statements
+
+/*
+	- Statements in programming are instructions that we tell the computer to perform.
+	- Js Statements usually end with a semicolon(;)
+	- Syntax in programming, it is the set of rules that describes how statements must be constructed.
+*/
+
+console.log("Hello Batch 138!");
+
+//Variables
+/*
+	- It is used to contain data.
+	- Declaring variables - tells our devices that a variable name is created and is ready to store data
+	- Syntax:
+		let/const variableName;
+	- let keyword - used to handle values that will change over time
+	- const [constant] keyword - used to handle values that will not change
+*/
+
+let myVariable;
+
+console.log(myVariable);
+
+// console.log(hello);
+
+// Initializing values to variables
+
+/*
+	-Syntax:
+	let/const variableName = value;
+*/
+
+let productName = 'desktop computer';
+
+console.log(productName);
+
+let productPrice = 18999;
+
+console.log(productPrice);
+
+/*
+	Reassigning variable values
+	- change the initial or previous value into another value
+*/
+
+productName = 'Laptop';
+
+console.log(productName);
